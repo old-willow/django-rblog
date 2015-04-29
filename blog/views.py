080ecb6def47bf121e9ad3e5fb5b116e0762fc10
Robert Kolozsi <robert.kolozsi@gmail.com>
2015-04-29 16:46:55 +0200

    Added basic setup of urls, view and template to list blogs.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.template import RequestContext
 
-# Create your views here.
+
+def blog_list(request):
+    context = {}
+    return render_to_response('blog/blog_list.html',
+                               context,
+                               context_instance=RequestContext(request))
