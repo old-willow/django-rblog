e4493198da4bf18d3694c280e3fcc229b304effc
Robert Kolozsi <robert.kolozsi@gmail.com>
2015-05-04 12:05:46 +0200

    Added Category and Link models.

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,21 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class Author(models.Model):
+    user = models.ForeignKey(User)
+    username = models.CharField('first name', max_length=100)
+    #firstname = models.CharField('first name', max_length=100)
+    #lastname = models.CharField('last name', max_length=100)
+
+    class Meta:
+        verbose_name = 'user name'
+        verbose_name_plural = 'user names'
+        ordering = ['username', ]
+
+    def __unicode__(self):
+        return self.user
 
 
 class Category(models.Model):
@@ -20,16 +36,32 @@ class Category(models.Model):
         return self.title
 
 
+class EntryPublishableQuerySet(models.QuerySet):
+    """
+    Returning blogs that are publishable.
+    """
+    def publishable(self):
+        return self.filter(publishable=True)
+
+class EntryNotPublishableQuerySet(models.QuerySet):
+    """
+    Returning blogs that are not yet publishable.
+    """
+    def not_publishable(self):
+        return self.filter(publishable=False)
+
+
 class Entry(models.Model):
     title = models.CharField('title',
                              max_length=255,
                              unique_for_date='pub_date',
                              help_text='title of the blog entry.')
+    author = models.ForeignKey(Author, verbose_name='author of the blog')
     slug = models.SlugField('slug',
                             help_text='slug field.')
-    category = models.ForeignKey(Category,
-                                 verbose_name='category',
-                                 help_text='Choosing a category or topic.')
+    category = models.ManyToManyField(Category,
+                                      verbose_name='category',
+                                      help_text='Choosing a category or topic.')
     publishable = models.BooleanField('is publishable?',
                                       default=False,
                                       help_text='is this blog entry publishable?')
@@ -39,11 +71,28 @@ class Entry(models.Model):
     modified_date = models.DateTimeField('modified date',
                                          auto_now=True,
                                          help_text='date when the blog entry was modified last time.')
-    entry = models.TextField('entry',
-                             help_text='the blog entry itself.')
+    body = models.TextField('entry',
+                            help_text='the blog entry itself.')
+    enable_comments = models.BooleanField('enable comments', default=False)
+    featured = models.BooleanField('featured', default=False)
+
+    LIVE_STATUS = 1
+    DRAFT_STATUS = 2
+    HIDDEN_STATUS = 3
+    STATUS_CHOICES = (
+        (LIVE_STATUS, 'Live'),
+        (DRAFT_STATUS, 'Draft'),
+        (HIDDEN_STATUS, 'Hidden'),
+    )
+    status = models.IntegerField('blog status', choices=STATUS_CHOICES,
+                                 default=DRAFT_STATUS)
+
+    objects = models.Manager()
+    published = EntryPublishableQuerySet().as_manager()
+    not_published = EntryNotPublishableQuerySet().as_manager()
 
     class Meta:
-        ordering = ['-pub_date']
+        ordering = ['-pub_date', ]
         verbose_name = 'entry'
         verbose_name_plural = 'entries'
 
@@ -59,14 +108,14 @@ class Link(models.Model):
     date_added = models.DateTimeField('date added',
                                       auto_now_add=True,
                                       help_text='date when the link is added to entry.')
-    entry = models.ManyToManyField('associated entry',
+    entry = models.ManyToManyField(Entry, verbose_name='associated entry',
                                   help_text='entry to which link is associated.')
     description = models.TextField('link description',
                                    blank=True, null=True, default=None,
                                    help_text='description of the link.')
 
     class Meta:
-        ordering = ['-date_added']
+        ordering = ['-date_added', ]
         verbose_name = 'link'
         verbose_name_plural = 'links'
 
